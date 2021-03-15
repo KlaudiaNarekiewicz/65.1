c2e2d558ec643c3c4ca5d8fb511350fdf8668375
KlaudiaNarekiewicz <62132418+KlaudiaNarekiewicz@users.noreply.github.com>
2021-03-15 09:57:54 +0100

    Add files via upload

---
+++ 65.1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class ulamki
+{
+	ifstream plik;
+	ofstream plik1;
+	
+	public:
+		ulamki();
+		~ulamki();
+		void wczytaj();
+};
+
+ulamki::ulamki() 
+{
+	plik.open("dane_ulamki.txt");
+	plik1.open("wynik.txt");
+}
+
+void ulamki::wczytaj() 
+{
+ 	double mian,licz;
+ 	double min, min_mian, min_licz;
+	if(plik.good()) 
+	{
+		plik>>mian;
+		plik>>licz;
+	
+		min = licz/mian;
+		min_licz = licz;
+		min_mian = mian;
+		while(!plik.eof()) 
+		{
+			
+			plik>>mian;	
+			plik>>licz;
+			if(licz/mian<min) 
+			{
+				min = licz/mian;
+				min_licz = licz;
+				min_mian= mian;
+			}
+			if(licz/mian==min) 
+			{
+				if(mian<=min_mian) 
+				{
+					min_licz = licz;
+					min_mian = mian;
+				}
+			}
+		}
+		cout<<"Zadanie"<<min_licz<<" "<<min_mian;
+		plik1<<"Zadanie"<<min_licz<<" "<<min_mian;
+	}
+}
+	
+ulamki::~ulamki() 
+{
+	plik.close();
+	plik1.close();
+}
+
+int main(int argc, char** argv) 
+{
+	ulamki s;
+	s.wczytaj();
+	
+	return 0;
+}
